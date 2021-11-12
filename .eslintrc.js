aaab323c9c2b800752e8716c864a5e823c9032f0
Ting.ye <ting.ye@MacBook-Pro.local>
2021-11-12 14:15:13 +0800

    chore: 配置commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  rules: {
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// let m = 'a'
+
+// const link = document.createElement('link')
+// link.rel = 'stylesheet'
+// link.href = `https://dl.dir.freefiremobile.com/common/web_event/powerbalance/${m}.css`
+// document.head.appendChild(link)
+
+// setTimeout(() => {
+//   m = 'b'
+//   link.href = `https://dl.dir.freefiremobile.com/common/web_event/powerbalance/${m}.css`
+// }, 3000)
+
 createApp(App).mount('#app')
